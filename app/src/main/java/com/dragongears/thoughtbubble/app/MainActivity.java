8ef1eb9ce84f47b6d11be6369a3ce1c05b1dd519
Art Dahm <art@dragongears.com>
2014-04-27 10:01:35 -0700

    Two activities

--- app/src/main/java/com/dragongears/thoughtbubble/app/MainActivity.java
+++ app/src/main/java/com/dragongears/thoughtbubble/app/MainActivity.java
@@ -1,10 +1,13 @@
 package com.dragongears.thoughtbubble.app;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,7 +18,6 @@ import android.widget.EditText;
 
 public class MainActivity extends ActionBarActivity {
     SharedPreferences preferences;
-    Boolean showingThought = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,49 +25,36 @@ public class MainActivity extends ActionBarActivity {
 
         preferences = PreferenceManager.getDefaultSharedPreferences(this);
 
-        Display display = ((WindowManager) this.getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        setContentView(R.layout.activity_main);
 
-        switch (display.getRotation()) {
-            case Surface.ROTATION_0:
-                showInputView();
-                break;
-            case Surface.ROTATION_180:
-                showInputView();
-                break;
-            case Surface.ROTATION_270:
-                showThoughtView();
-                break;
-            case Surface.ROTATION_90:
-                showThoughtView();
-                break;
-        }
-
+        EditText et = (EditText) findViewById(R.id.editText);
+        et.setText(preferences.getString("pref_thought_text", "Default value"));
     }
 
     @Override
     protected void onDestroy () {
         super.onDestroy();
 
-        if (!showingThought) {
-            EditText et = (EditText) findViewById(R.id.editText);
-            SharedPreferences.Editor preferencesEditor = preferences.edit();
-            preferencesEditor.putString("pref_thought_text", et.getText().toString());
-            preferencesEditor.commit();
-        }
+        EditText et = (EditText) findViewById(R.id.editText);
+        SharedPreferences.Editor preferencesEditor = preferences.edit();
+        preferencesEditor.putString("pref_thought_text", et.getText().toString());
+        preferencesEditor.apply();
     }
 
-    private void showInputView() {
-        showingThought = false;
-        setContentView(R.layout.activity_main);
-    }
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
 
-    private void showThoughtView() {
-        showingThought = true;
-        setContentView(R.layout.activity_main_rotate);
-        AutoResizeTextView artv = (AutoResizeTextView) findViewById(R.id.thoughtText);
-        artv.setText(preferences.getString("pref_thought_text", "Default text"));
-    }
+        //Starting a new Intent
+        Intent intent = new Intent(this, ThoughtActivity.class);
 
+        EditText et = (EditText) findViewById(R.id.editText);
+        //Sending data to another Activity
+        intent.putExtra("message", et.getText().toString());
+
+        startActivity(intent);
+        finish();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
